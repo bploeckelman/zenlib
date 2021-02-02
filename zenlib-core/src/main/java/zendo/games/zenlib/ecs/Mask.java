a525f409e68a81f4a1abdbf1ab85a9e5a86c52ef
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2021-02-02 02:45:51 -0600

    Add additional mask for jumpthrough platforms

--- zenlib-core/src/main/java/zendo/games/zenlib/ecs/Mask.java
+++ zenlib-core/src/main/java/zendo/games/zenlib/ecs/Mask.java
@@ -1,7 +1,8 @@
 package zendo.games.zenlib.ecs;
 
 public class Mask {
-    public static final int solid = 1 << 0;
-    public static final int enemy = 1 << 1;
+    public static final int solid         = 1 << 0;
+    public static final int enemy         = 1 << 1;
     public static final int player_attack = 1 << 2;
+    public static final int jumpthrough   = 1 << 3;
 }
